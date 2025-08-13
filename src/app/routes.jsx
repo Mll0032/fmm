c551435231f1e2cfea84df33cc3b8a49e86dee3b
Mll0032 <mllawson18@gmail.com>
2025-08-13 12:42:49 -0500

    update locaation and import of App.jsx
    add routes.jsx
    
    Update file structure for future scalability

---
+++ src/app/routes.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "../pages/Dashboard.jsx";
+import Modules from "../pages/Modules.jsx";
+import Library from "../pages/Library.jsx";
+import Settings from "../pages/Settings.jsx";
+import About from "../pages/About.jsx";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/modules" element={<Modules />} />
+      <Route path="/library" element={<Library />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+}
