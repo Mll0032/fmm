93228483f198e2d3461e44d1e3caf01395369315
Mll0032 <mllawson18@gmail.com>
2025-08-19 20:16:47 -0500

    Adding global data for smoother movement though the app.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Layout from "../components/Layout/Layout.jsx";
 import AppRoutes from "./routes.jsx";
+import { DataProvider } from "../context/DataContext.jsx";
 
 export default function App() {
   return (
-    <>
+    <DataProvider>
       <a className="skip-link" href="#main">Skip to content</a>
       <Layout>
         <AppRoutes />
       </Layout>
-    </>
+    </DataProvider>
   );
 }
