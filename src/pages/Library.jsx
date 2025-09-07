835662584d34e9c316ce0b5824ca4f030fd8e8d7
Mll0032 <mllawson18@gmail.com>
2025-09-07 14:05:14 -0500

    Update the Library to say coming soon.

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -2,7 +2,7 @@ export default function Library() {
   return (
     <section style={{ padding: "20px 0" }}>
       <h2>Library</h2>
-      <p>Store your encounters, NPCs, items, and reusable section templates.</p>
+      <p>Coming Soon Store your encounters, NPCs, items, and reusable section templates. Make them public or private</p>
     </section>
   );
 }
