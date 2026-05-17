7268ec1651bb64dd91c3d6876f36ad7b189d0ce0
Mll0032 <mllawson18@gmail.com>
2026-05-17 13:28:11 -0500

    Touch functions added to Modules

--- src/pages/Modules.jsx
+++ src/pages/Modules.jsx
@@ -2,15 +2,16 @@ import React, { useMemo, useState, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { useData } from "../hooks/useData.js";
 
-// Hover-enabled button component
+const isTouch = window.matchMedia("(pointer: coarse)").matches;
+
 function HoverButton({ children, onClick, style, hoverStyle, ...props }) {
   const [isHovered, setIsHovered] = useState(false);
-  
+
   return (
     <button
       onClick={onClick}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
+      onMouseEnter={isTouch ? undefined : () => setIsHovered(true)}
+      onMouseLeave={isTouch ? undefined : () => setIsHovered(false)}
       style={isHovered ? hoverStyle : style}
       {...props}
     >
