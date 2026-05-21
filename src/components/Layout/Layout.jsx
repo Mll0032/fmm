bd24127e99df1c83a23a9c596d361987c0c60f90
Mll0032 <mllawson18@gmail.com>
2026-05-21 18:46:56 -0500

    Adding settings to the layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Header from "../Header/Header.jsx";
 import styles from "./Layout.module.css";
+import { useSettingsSync } from "../../hooks/useSettingsSync.js";
 
 export default function Layout({ children }) {
+  useSettingsSync();
   return (
     <div className={styles.shell}>
       <Header />
